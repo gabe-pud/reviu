094ddf9bf927877d5d94c42f2f235959e7c4a221
gabe-pud <gabrielgarciasantana3@gmail.com>
2025-11-25 04:25:00 -0300

    Refactor CSS styles and improve HTML structure
    - Removed unused color variables from output.css.
    - Added new height and min-height utility classes in output.css.
    - Introduced flex-shrink utility classes in output.css.
    - Enhanced grid column utilities in output.css.
    - Updated gap utilities in output.css.
    - Added rounded-lg utility class in output.css.
    - Introduced object-contain class in output.css.
    - Added shadow-sm utility class in output.css.
    - Cleaned up media query styles in output.css.
    - Improved navigation button logic in base.html to prioritize current path.
    - Simplified deck selection logic in deck.html and improved button structure.
    - Refactored card rendering logic in deck.html to use a loop for dynamic content.
    - Enhanced modal forms for adding and editing cards in deck.html.
    - Updated AJAX calls to standard form submissions in deck.html.
    - Improved the main page index.html to link to deck creation with enhanced UI.
    - Added JavaScript to handle navigation animation when adding a new card from the main page.

--- reviu/decks/urls.py
+++ reviu/decks/urls.py
@@ -5,5 +5,10 @@ from . import views
 
 urlpatterns = [
     path('decks/', views.decks_view, name='decks'),
+    path('decks/crate', views.criar_deck, name="criar_deck"),
+    path('decks/<int:deck_id>/delete/',views.deletar_deck, name="criar_deck"),
+    path('decks/<int:deck_id>/cards/',  views.criar_card, name="criar_card"),
+    path('decks/<int:deck_id>/cards/<int:card_id>/edit/', views.alterar_card, name="alterar_card"),
+    path('decks/<int:deck_id>/cards/<int:card_id>/delete/', views.deletar_card, name="deletar_card"),
 ]
 
--- reviu/decks/views.py
+++ reviu/decks/views.py
@@ -1,6 +1,198 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from datetime import datetime, timedelta, date
 
 # Create your views here.
 
 def decks_view(request):
-    return render(request, 'decks/deck.html')
+    if request.session.get('auth_token') == None:
+        return redirect('login')
+    
+    headers={
+        "Authorization":request.session.get('auth_token')
+    }
+
+    try:
+        response = requests.get('http://localhost:8080/decks',headers=headers)
+        response.raise_for_status() 
+        dados_api = response.json()
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao buscar decks da API: {e}")
+        dados_api = {'decks': []}
+    
+    decks = []
+    current_deck_id = request.POST.get("current_deck_id")
+    current_deck_name = ""
+
+    for i in dados_api:
+        deck = {
+            "id": i.get("id"),
+            "name": i.get("name")
+        }
+        if request.POST.get("current_deck_id") is not None:
+            if i.get("id") == int(request.POST.get("current_deck_id")):
+                current_deck_name = i.get("name")
+        decks.append(deck)
+        
+    
+    if request.method == "POST":
+        hoje = date.today()
+
+        try:
+            response = requests.get('http://localhost:8080/decks/'+current_deck_id+'/cards',headers=headers)
+            response.raise_for_status()
+            dados_api = response.json()
+
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao buscar decks da API: {e}")
+            dados_api = {'cards': []}
+        
+        cards = []
+
+        for i in dados_api:
+            data_revisao = datetime.strptime(i.get("nextReview"), '%Y-%m-%d').date()
+            if data_revisao <= hoje:
+                data_revisao = "hoje"
+            elif data_revisao == hoje + timedelta(days=1):
+                data_revisao = "amanhã"
+            else:
+                data_revisao = data_revisao.strftime('%d/%m')
+            
+            card = {
+                "id": i.get("id"),
+                "frontText": i.get("frontText"),
+                "backText": i.get("backText"),
+                "nextReview": data_revisao,
+                "imageUrl": i.get("imageUrl"),
+                "audioUrl": i.get("audioUrl"),
+            }
+            cards.append(card)
+    else:
+        cards = []
+
+    print(decks)
+    context = {
+        "decks":decks,
+        "current_deck_id": current_deck_id,
+        "current_deck_name": current_deck_name,
+        "cards":cards
+    }
+    return render(request, 'decks/deck.html', context)
+
+
+def criar_deck(request):
+    headers={
+        "Authorization":request.session.get('auth_token')
+    }
+
+    data = {
+        "name": request.POST.get("name")
+    }
+
+    try:
+        response = requests.post("http://localhost:8080/decks",headers=headers,json=data)
+        response.raise_for_status()
+        dados_api = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao buscar decks da API: {e}")
+        dados_api = {'cards': []}
+    
+    print(dados_api)
+
+    return redirect("decks")
+
+
+def deletar_deck(request,deck_id):
+    url = "http://localhost:8080/decks/"+str(deck_id)
+
+    headers={
+        "Authorization":request.session.get('auth_token')
+    }
+
+    try:
+        response = requests.delete(url,headers=headers)
+        response.raise_for_status()
+        dados_api = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao buscar decks da API: {e}")
+        dados_api = {'cards': []}
+    
+    print(dados_api)
+
+    return redirect("decks")
+
+
+def criar_card(request, deck_id):
+    url = "http://localhost:8080/decks/"+str(deck_id)+"/cards"
+
+    headers={
+        "Authorization":request.session.get('auth_token')
+    }
+
+    data = {
+        "frontText": request.POST.get("frontText"),
+        "backText": request.POST.get("backText"),
+    }
+
+    try:
+        response = requests.post(url,headers=headers,json=data)
+        response.raise_for_status()
+        dados_api = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao buscar decks da API: {e}")
+        dados_api = {'cards': []}
+    
+    print(dados_api)
+
+    return redirect("decks")
+
+
+def alterar_card(request, deck_id, card_id):
+    url = "http://localhost:8080/decks/"+str(deck_id)+"/cards/"+str(card_id)
+
+    headers={
+        "Authorization":request.session.get('auth_token')
+    }
+
+    data = {
+        "frontText": request.POST.get("frontText"),
+        "backText": request.POST.get("backText")
+    }
+
+    try:
+        response = requests.put(url,headers=headers,json=data)
+        response.raise_for_status()
+        dados_api = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao buscar decks da API: {e}")
+        dados_api = {'cards': []}
+    
+    print(dados_api)
+
+    return redirect("decks")
+
+
+def deletar_card(request, deck_id, card_id):
+    url = "http://localhost:8080/decks/"+str(deck_id)+"/cards/"+str(card_id)
+
+    headers={
+        "Authorization":request.session.get('auth_token')
+    }
+
+    try:
+        response = requests.delete(url,headers=headers)
+        response.raise_for_status()
+        dados_api = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao buscar decks da API: {e}")
+        dados_api = {'cards': []}
+    
+    print(dados_api)
+    return redirect("decks")
+
